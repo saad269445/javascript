cd3e2ae535107e65761b20830f854dffb4f30682
saad269445 <102538252+saad269445@users.noreply.github.com>
2022-05-23 00:54:50 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+//Question # 01/02/03/04
+ 
+var username;
+var MyName = "Full Name";
+
+var g  = "Hello World";
+
+var a = "Jhone Doe";
+
+alert(a);
+
+var b = "15 years old";
+alert(b);
+
+var c = "certified mobile application development"
+alert(c);
+
+
+//Question#05
+
+var a = "pizza"
+var b = "pizz"
+var c = "piz"
+var d = "pi"
+var e = "p"
+var d = a + "\n" + b + "\n" + c + "\n" + d + "\n" + e ;
+alert (d);
+
+
+
+//Question#06
+
+var a = "My email address is example@example.com";
+alert(a);
+
+
+
+//Question#07
+
+var a = "I am trying to learn from the book A smaller" + "\n" +
+        "way to learn Javascript";
+alert (a);
+
+
+//Question#08
+
+document.write("Yah! I can write HTML contact JavaScript");
+
+
+
+//Question#09
+
+var a = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(a);
